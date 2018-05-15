47e25bb74ce4a49ad1f8cdcbad81aa7e1af8d53a
David Matellano <david.matellano@ffptn396.local>
2018-05-15 19:22:52 +0100

    first commit

---
+++ Sources/App/Models/Acronym.swift
@@ -0,0 +1,24 @@
+//
+//  Acronym.swift
+//  App
+//
+//  Created by David Matellano on 15/05/2018.
+//
+
+import Vapor
+import FluentSQLite
+final class Acronym: Codable {
+    var id: Int?
+    var short: String
+    var long: String
+    init(short: String, long: String) {
+        self.short = short
+        self.long = long
+    }
+}
+
+extension Acronym: SQLiteModel {}
+
+extension Acronym: Migration {}
+
+extension Acronym: Content {}
